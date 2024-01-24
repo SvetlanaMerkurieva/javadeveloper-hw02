b26595614126d99963c871530b590f8ac68fdd67
SvetlanaMerkurieva <82316953+SvetlanaMerkurieva@users.noreply.github.com>
2024-01-24 20:04:37 +0200

    Adds edits

--- src/main/java/tictactoe/TicTacToe.java
+++ src/main/java/tictactoe/TicTacToe.java
@@ -4,13 +4,9 @@ import java.util.Scanner;
 
 
 public class TicTacToe {
+    private static final Scanner scan = new Scanner(System.in);
 
-    public void main () {
-        Scanner scan = new Scanner(System.in);
-        byte input;
-        byte rand;
-        byte i;
-        boolean boxAvailable;
+    public void playTicTacToe () {
         char[] box = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
         int numberOfBoxes = 9;
         System.out.println(("Enter box number to select. Enjoy!\n"));
@@ -22,8 +18,7 @@ public class TicTacToe {
             System.out.println("-----------");
             System.out.println(" " + box[6] + " | " + box[7] + " | " + box[8] + " \n");
             if (!boxEmpty) {
-                for (i = 0; i < numberOfBoxes; i++)
-                    box[i] = ' ';
+                putSpace(box, numberOfBoxes);
                 boxEmpty = true;
             }
 
@@ -36,41 +31,24 @@ public class TicTacToe {
                 break;
             }
 
-            boxAvailable = false;
-            for (i = 0; i < numberOfBoxes; i++) {
-                if (isBoxFree(box, i)) {
-                    boxAvailable = true;
-                    break;
-                }
-            }
-            if (!boxAvailable) {
+            if (!isBoxAvailable(box, numberOfBoxes)) {
                 System.out.println("It's a draw!\nCreated by Shreyas Saha. Thanks for playing!");
                 break;
             }
 
-            while (true) {
-                input = scan.nextByte();
-                if (isCorrectInput(input, numberOfBoxes)) {
-                    if (isBoxNotFree(box, input)) {
-                        System.out.println("That one is already in use. Enter another.");
-                    } else {
-                        box[input - 1] = 'X';
-                        break;
-                    }
-                } else {
-                    System.out.println("Invalid input. Enter again.");
-                }
-            }
+            putCross(scan, box, numberOfBoxes);
 
-            while (true) {
-                rand = (byte) (Math.random() * (9 - 1 + 1) + 1);
-                if (isRandBoxFree(box, rand)) {
-                    box[rand - 1] = 'O';
-                    break;
-                }
-            }
+            putZero(box);
         }
     }
+
+    public static char[] putSpace(char[] box, int numberOfBoxes) {
+        for (byte i = 0; i < numberOfBoxes; i++) {
+            box[i] = ' ';
+        }
+        return box;
+    }
+
 
     public static boolean isThreeCrossesInRow (char[] box) {
         return (box[0]=='X' && box[1]=='X' && box[2]=='X') || (box[3]=='X' && box[4]=='X' && box[5]=='X') || (box[6]=='X' && box[7]=='X' && box[8]=='X') ||
@@ -84,10 +62,6 @@ public class TicTacToe {
                 (box[0]=='O' && box[4]=='O' && box[8]=='O') || (box[2]=='O' && box[4]=='O' && box[6]=='O');
     }
 
-    public static boolean isRandBoxFree (char[] box, byte rand) {
-        return box[rand - 1] != 'X' && box[rand - 1] != 'O';
-    }
-
     public static boolean isBoxFree (char[] box, byte i) {
         return box[i] != 'X' && box[i] != 'O';
     }
@@ -96,8 +70,49 @@ public class TicTacToe {
         return box[input - 1] == 'X' || box[input - 1] == 'O';
     }
 
+    public static boolean isBoxAvailable(char[] box, int numberOfBoxes) {
+        boolean boxAvailable = false;
+        for (byte i = 0; i < numberOfBoxes; i++) {
+            if (isBoxFree(box, i)) {
+                boxAvailable = true;
+                break;
+            }
+        }
+        return boxAvailable;
+    }
     public static boolean isCorrectInput (byte input, int numberOfBoxes) {
         return input > 0 && input <= numberOfBoxes;
+    }
+
+    public static char[] putCross(Scanner scan, char[] box, int numberOfBoxes) {
+        while (true) {
+            byte input = scan.nextByte();
+            if (isCorrectInput(input, numberOfBoxes)) {
+                if (isBoxNotFree(box, input)) {
+                    System.out.println("That one is already in use. Enter another.");
+                } else {
+                    box[input - 1] = 'X';
+                    break;
+                }
+            } else {
+                System.out.println("Invalid input. Enter again.");
+            }
+        }
+            return box;
+    }
+
+    public static char[] putZero(char[] box) {
+        while (true) {
+            byte rand = (byte) (Math.random() * (9 - 1 + 1) + 1);
+            if (isRandBoxFree(box, rand)) {
+                box[rand - 1] = 'O';
+                break;
+            }
+        }
+        return box;
+    }
+    public static boolean isRandBoxFree (char[] box, byte rand) {
+        return box[rand - 1] != 'X' && box[rand - 1] != 'O';
     }
 
 }
